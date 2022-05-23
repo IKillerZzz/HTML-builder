b5a0fe8df5e5b1691a81620e4f3e237c726fe111
Ilya Pol <aliveshooter@gmail.com>
2022-05-23 15:40:23 +0300

    refactor: resolve 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const fs = require('fs');
 const filePath = path.join(__dirname, 'text.txt');
+const stream = new fs.ReadStream(filePath, { encoding: 'utf-8' });
 
-fs.readFile(filePath, 'utf-8', (err, content) => {
-  if (err) {
-    throw err;
-  }
-  console.log(content);
+stream.on('error', (err) => { throw err; });
+
+stream.on('readable', () => {
+  console.log(stream.read());
+});
+
+stream.on('end', () => {
+  console.log('Чтение файла завершено.');
 });
